ba9a0ca4218e7719cfc3e3ddc306d2d3ed609a27
PhoenixGS <thestarrydream@gmail.com>
2024-05-31 00:28:21 +0800

    Add Exp2-3

---
+++ Exp2/P3.m
@@ -0,0 +1,10 @@
+cnt = 0;
+l = 0;
+while cnt < 10
+    r = l + 0.1;
+    if besselj(0, l)*besselj(0, r) < 0
+        fzerotx(@(x) besselj(0, x), [l, r])
+        cnt = cnt + 1;
+    end
+    l = l + 0.1;
+end
